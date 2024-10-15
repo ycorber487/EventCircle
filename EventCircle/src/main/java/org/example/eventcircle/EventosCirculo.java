8c5026a6d201e1f9d31a9acb5b614a52d39eae34
ycorber487 <163828691+ycorber487@users.noreply.github.com>
2024-10-15 18:33:00 +0200

    Update EventosCirculo.java

--- EventCircle/src/main/java/org/example/eventcircle/EventosCirculo.java
+++ EventCircle/src/main/java/org/example/eventcircle/EventosCirculo.java
@@ -11,31 +11,31 @@ public class EventosCirculo extends Application {
     @Override
     public void start(Stage stage) {
 
-        GridPane pane = new GridPane();
-        Scene scene = new Scene(pane, 400, 500);
-        stage.setTitle("Play with circle");
+        GridPane pane = new GridPane(); // definimos un contenedor tabla
+        Scene scene = new Scene(pane, 400, 500); // definimos el tamanio de la escena e introducimos el tamanio
+        stage.setTitle("Play with circle"); // le damos un titulo
 
 
-        Circle circulo = new Circle();
-        circulo.setCenterX(100);
+        Circle circulo = new Circle(); // creamos un circulo
+        circulo.setCenterX(100); // centro en el 100,100 y radio de 20
         circulo.setCenterY(100);
         circulo.setRadius(20);
 
 
-        pane.add(circulo, 1, 1);
+        pane.add(circulo, 1, 1); // colocamos el circulo en la posicion 1,1
 
 
-        Button agrandar = new Button("Agrandar");
-        agrandar.setOnAction(e -> circulo.setRadius(circulo.getRadius() + 5));
+        Button agrandar = new Button("Agrandar"); // creamos el boton que agranda
+        agrandar.setOnAction(e -> circulo.setRadius(circulo.getRadius() + 5)); //aumentamos el radio 5 puntos
 
-        Button disminuir = new Button("Disminuir");
+        Button disminuir = new Button("Disminuir"); // creamos el boton que empequeñece
         disminuir.setOnAction(e -> {
-                circulo.setRadius(circulo.getRadius() - 5);
+                circulo.setRadius(circulo.getRadius() - 5); //reducimos el radio 5 puntos
 
         });
         
-        pane.add(agrandar, 0, 2);
-        pane.add(disminuir, 1, 2);
+        pane.add(agrandar, 0, 2); // agregamos el boton agrandar en la 0 2
+        pane.add(disminuir, 1, 2);// agregamos el boton disminuir en la 1,2
 
 
         stage.setScene(scene);
